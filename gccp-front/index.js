00981ff9947072b7499ec450a03fb639d1bb5dfe
Mendes Sousa <leoprofessionallogytech@outlook.com>
2022-08-27 03:20:35 -0300

    program almost finished

--- gccp-front/index.js
+++ gccp-front/index.js
@@ -3,6 +3,15 @@ const doc=document
 function load(){
     const URL='http://localhost:3003'
 
-    fetch(URL,{method:'GET'}).then(response=>console.log(response.json()))
+    fetch(URL,{method:'GET'}).then(response=>response.json())
+                             .then(response=>{
+                                response.map(e=>{
+                                    let idDb=e.id
+                                    let msgDb=e.msg
+
+                                    
+                                })
+                             })
+                             .catch(err=>console.log(err))
 }
 load()  
